6876edf8b8595d3ccb0832d7ad017aca5f997066
danoan <danoan2008@gmail.com>
2019-12-09 15:04:57 +0100

    II-5 plot script comparison across shapes

---
+++ chapter5/flow/scripts/plot-scripts/summary-ii5.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+source "${SCRIPT_PATH}/graph-plot.sh"
+
+SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+DATA_FOLDER=$( realpath $1 )
+OUTPUT_FOLDER=$( realpath $2 )
+LENGTH_PEN=0.01000
+ENERGY=elastica
+
+mkdir -p $OUTPUT_FOLDER
+
+shapes="bean square flower triangle ellipse"
+
+prefix_input()
+{
+    shape=$1
+    radius=$2
+    estimator=$3
+    length_pen=$4
+    gs=$5
+    energy=$6
+
+    echo $DATA_FOLDER/$shape/radius_$radius/$estimator/$energy/len_pen_$length_pen/m2M50/jonctions_1/best/gs_$gs
+}
+
+
+create_multiplot "$OUTPUT_FOLDER/summary-ii5.eps" "$s" "$( prefix_input triangle 5 ii $LENGTH_PEN 0.25000 $ENERGY )/energy.txt" "triangle" \
+"$( prefix_input square 5 ii $LENGTH_PEN 0.25000 $ENERGY )/energy.txt" "square" \
+"$( prefix_input flower 5 ii $LENGTH_PEN 0.25000 $ENERGY )/energy.txt" "flower" \
+"$( prefix_input bean 5 ii $LENGTH_PEN 0.50000 $ENERGY )/energy.txt" "bean"
