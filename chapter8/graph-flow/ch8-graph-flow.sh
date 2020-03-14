25cffab1c7e3db5ded98f682f5b58ad1ed61b3f9
danoan <danoan2008@gmail.com>
2020-03-14 11:53:55 +0100

    graph-cut chapter figures update

--- chapter8/graph-flow/ch8-graph-flow.sh
+++ chapter8/graph-flow/ch8-graph-flow.sh
@@ -14,14 +14,18 @@ flow_and_summary()
 {
     shape=$1
     neigh=$2
-    DATA_FOLDER=$3
-    OUTPUT_FOLDER=$4
+    alpha=$3
+    DATA_FOLDER=$4
+    OUTPUT_FOLDER=$5
+
+    ropt=$( python3 -c "print('{}'.format(1.0/pow($alpha,0.5)))" )
+#    echo "$shape - $neigh - $alpha - $DATA_FOLDER - $OUTPUT_FOLDER"
 
     mkdir -p $DATA_FOLDER
     mkdir -p $OUTPUT_FOLDER
 
-    ${GRAPH_FLOW_APP} -r9 -i400 -S${shape} -N${neigh} $DATA_FOLDER
-    ${SUMMARY_APP} $DATA_FOLDER ${OUTPUT_FOLDER}/summary.eps -j10
+    ${GRAPH_FLOW_APP} -r9 -i400 -S${shape} -a${alpha} -B${ropt} -N${neigh} $DATA_FOLDER
+    ${SUMMARY_APP} $DATA_FOLDER ${OUTPUT_FOLDER}/summary.eps -j10 -r${ropt} -h0.25
 
     epstopdf ${OUTPUT_FOLDER}/summary.eps
     rm ${OUTPUT_FOLDER}/summary.eps
@@ -30,17 +34,17 @@ flow_and_summary()
 }
 
 
-SHAPE="triangle flower bean"
 NEIGH="0 2"
+ALPHA="0.01 0.001"
 
 
-for shape in $SHAPE
+for neigh in $NEIGH
 do
-    for neigh in $NEIGH
+    for alpha in $ALPHA
     do
-        DATA_FOLDER=${BASE_DATA_FOLDER}/${shape}/neigh-${neigh}
-        OUTPUT_FOLDER=${BASE_OUTPUT_FOLDER}/${shape}/neigh-${neigh}
+        DATA_FOLDER=${BASE_DATA_FOLDER}/triangle/neigh-${neigh}/alpha-${alpha}
+        OUTPUT_FOLDER=${BASE_OUTPUT_FOLDER}/triangle/neigh-${neigh}/alpha-${alpha}
 
-        flow_and_summary $shape $neigh $DATA_FOLDER $OUTPUT_FOLDER&
+        flow_and_summary "triangle" $neigh $alpha $DATA_FOLDER $OUTPUT_FOLDER &
     done
 done
--- ext-projects/cmake-files/master/graph-flow.cmake
+++ ext-projects/cmake-files/master/graph-flow.cmake
@@ -13,8 +13,12 @@ ExternalProject_Add(graph-flow
         -DGCURVE_INCLUDE_DIRS=${EXTPROJECTS_BUILD_DIR}/include
         -DGCURVE_LIBS_DIR=${EXTPROJECTS_BUILD_DIR}/lib
         -DGEOC_INCLUDE_DIRS=${EXTPROJECTS_BUILD_DIR}/include
+        -DBTOOLS_INCLUDE_DIRS=${EXTPROJECTS_BUILD_DIR}/include
+        -DBTOOLS_LIBS_DIRS=${EXTPROJECTS_BUILD_DIR}/lib
         -DMAGLAC_INCLUDE_DIRS=${EXTPROJECTS_BUILD_DIR}/include
         -DLEMON_INCLUDE_DIRS=${CMAKE_PREFIX_PATH}/include
         -DLEMON_LIBS_DIRS=${CMAKE_PREFIX_PATH}/lib
         -DBOOST_INCLUDE_DIRS=${BOOST_INCLUDE_DIRS}
         -DBOOST_LIBS_DIR=${BOOST_LIBS_DIR})
+
+add_dependencies(graph-flow dipacus geoc gcurve magLac btools)
